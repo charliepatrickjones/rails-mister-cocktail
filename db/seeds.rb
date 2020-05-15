1d876f4dedec3d5d5076571182698a8cda600bd8
Charlie Jones <charlie@kitchup.co.uk>
2020-05-15 11:01:11 +0100

    create cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Cocktail.create(name: "lemon")
+Cocktail.create(name: "Espresso Martini")
+Cocktail.create(name: "Dark & Stormy")
+Cocktail.create(name: "Moscow Mule")
